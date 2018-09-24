f6cad22bf63d6b69566e97ba319d119b5496f217
j-nasu <speamint6901@gmail.com>
2018-09-24 18:29:16 +0900

    ちょっと修正

--- import.sh
+++ import.sh
@@ -1,6 +1,6 @@
 #bin/sh
 
 cd laradock
-cp ../hammocks/dump/$1.sql .
-docker-compose exec mysql mysql -uroot -p hammocks -e"$(cat $1.sql)"
-rm -rf $1.sql
+cp ../hammocks/dump/$1 .
+docker-compose exec mysql mysql -uroot -p hammocks -e"$(cat $1)"
+rm -rf $1
